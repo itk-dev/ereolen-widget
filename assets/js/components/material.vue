d339b854cd8021b81f9331519f11d873b0d841a0
Mikkel Ricky <rimi@aarhus.dk>
2018-10-01 13:56:20 +0200

    ER-93: Cleaned up authors list

--- assets/js/components/material.vue
+++ assets/js/components/material.vue
@@ -8,44 +8,54 @@
             </div>
             <div class="result-title">
                 <p><strong>{{ title }}</strong></p>
-                <p v-if="data.contributors">Af: <span v-for="(contributor, index) in data.contributors" v-bind:key="contributor">{{ contributor }}<span v-if="index < data.contributors.length - 1">, </span></span></p>
+                <p v-if="authors">Af: <span v-for="(author, index) in authors" v-bind:key="author">{{ author }}<span v-if="index < authors.length - 1">, </span></span></p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-   export default {
+    export default {
         name: 'Material',
         props: {
-          data: {
-            type: Object,
-            required: true
-          },
-          id: {
-            type: String,
-            required: true
-          },
-          title: {
-            type: String,
-            required: true
-          },
-          cover: {
-            type: String,
-            required: true
-          },
-          url: {
-            type: String,
-            required: true
-          },
-          icon: {
-            type: String,
-            required: true
-          },
-          action: {
-            type: Function,
-            required: true
-          }
+            data: {
+                type: Object,
+                required: true
+            },
+            id: {
+                type: String,
+                required: true
+            },
+            title: {
+                type: String,
+                required: true
+            },
+            cover: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            },
+            icon: {
+                type: String,
+                required: true
+            },
+            action: {
+                type: Function,
+                required: true
+            }
+        },
+        computed: {
+            authors: function() {
+                if (this.data.contributors.length > 0) {
+                    return this.data.contributors;
+                } else if (this.data.creators.length > 0) {
+                    return this.data.creators;
+                }
+                return null;
+            }
         }
     }
 </script>
