1e35ed99736c3de7af5787c8c028e3c85ea2221f
Mikkel Ricky <rimi@aarhus.dk>
2019-01-10 10:08:35 +0100

    Removed unused variable

--- src/Service/WidgetContextService.php
+++ src/Service/WidgetContextService.php
@@ -21,29 +21,6 @@ class WidgetContextService
     /** @var \App\Repository\WidgetContextRepository */
     private $repository;
 
-    private $contexts = [
-        'ereolen' => [
-            'name' => 'ereolen',
-            'host' => 'widget.ereolen.dk',
-            'label' => 'eReolen',
-            'url' => 'https://www.ereolen.dk',
-            'searchLink' => '<a href="https://ereolen.dk/search/ting" target="_blank">eReolen</a>',
-            'logo' => '/images/eReolen_Logo.svg',
-            'searchUrl' => 'https://www.ereolen.dk/search/ting',
-            'ereol_widget_search_url' => 'https://itk:itk@stg.ereolen.dk/widget/search',
-        ],
-        'ereolengo' => [
-            'name' => 'ereolengo',
-            'host' => 'widget.ereolengo.dk',
-            'label' => 'eReolen GO',
-            'url' => 'https://www.ereolengo.dk',
-            'searchLink' => '<a href="https://ereolengo.dk/search/ting" target="_blank">eReolen GO</a>',
-            'logo' => '/images/eReolenGo_Logo.svg',
-            'searchUrl' => 'https://www.ereolengo.dk/search/ting',
-            'ereol_widget_search_url' => 'https://itk:itk@stg.ereolengo.dk/widget/search',
-        ],
-    ];
-
     public function __construct(WidgetContextRepository $repository, RequestStack $requestStack)
     {
         $this->repository = $repository;
